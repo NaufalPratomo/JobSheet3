2f6b7769964c0e6a1d1a9dbc1e6f66a64b6e2179
Naufal Pratomo <naufalpratomo8@gmail.com>
2023-09-12 20:26:13 +0700

    first commit

---
+++ ContohVariabel19.java
@@ -0,0 +1,14 @@
+public class ContohVariabel19 {
+    public static void main(String[] args) {
+        String hobbyFavoriteSayaYaitu = "Bermain petak umpet";
+        boolean isPandaiMe = true;
+        char jenisKelaminSaya = 'L';
+        byte umurSayaSaatIni = 20;
+        double $ipkSaya = 3.24, tinggiMe = 1.78;
+        System.out.println(hobbyFavoriteSayaYaitu);
+        System.out.println("Apakah pandai? "+ isPandaiMe);
+        System.out.println("Jenis kelamin: "+ jenisKelaminSaya);
+        System.out.println("Umurku saat ini: " + umurSayaSaatIni);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipkSaya, tinggiMe));
+    }
+}
